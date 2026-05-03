54e06f4e4522326aa37c990e6b7ba04cb4228a00
Bard <bardsoleim@proton.com>
2026-05-03 16:17:17 +0200

    Fix Windows: use CreateDirectoryW instead of _mkdir

--- src/mingwMain/kotlin/platform.kt
+++ src/mingwMain/kotlin/platform.kt
@@ -6,12 +6,12 @@ import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.serialization.kotlinx.json.*
 import kotlinx.cinterop.ExperimentalForeignApi
 import kotlinx.serialization.json.Json
-import platform.posix._mkdir
+import platform.windows.CreateDirectoryW
 
 actual fun buildHttpClient(json: Json): HttpClient = HttpClient(WinHttp) {
     install(ContentNegotiation) { json(json) }
 }
 
 @OptIn(ExperimentalForeignApi::class)
-actual fun makeDir(path: String) { _mkdir(path) }
+actual fun makeDir(path: String) { CreateDirectoryW(path, null) }
 
